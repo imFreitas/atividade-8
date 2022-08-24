a3542eee50a20ec09d9b6614d4d0728c742f62c8
Pedro Henrique de Freitas <110676473+imFreitas@users.noreply.github.com>
2022-08-24 17:17:40 -0300

    Add files via upload

---
+++ exercicio 4/areas.js
@@ -0,0 +1,18 @@
+// CIRCULO
+var raio = prompt("Raio do circulo: ");
+
+raio = parseFloat(raio);
+
+var area = Math.PI * raio * raio;
+
+alert("Area da circunferencia = "+ area);
+// /////////////////////////////////////////////////////////////////////////////////////
+
+// TRIANGULO
+
+var base = prompt("Base do triangulo");
+var altura = prompt("Altura do Triangulo");
+
+var area = base * altura/2
+
+alert ("Area do triangulo = "+ area)
---
+++ exercicios1-3/Atividade 1-3.js
@@ -0,0 +1,13 @@
+var NomeDoCarro = "fusca"
+
+// questão 1
+
+
+var mensagem = "Olá "
+var nome = prompt("Digite seu nome") 
+var idade = prompt("Digite sua idade")
+var mensagem_idade = (" sua idade é ")
+
+alert(mensagem + nome + mensagem_idade + idade);
+
+// questão 2 e 3
